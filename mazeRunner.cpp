4a8a2a1c33e8744a4bb73e7112ad3696d4b5c768
MarkoCukanic <s4008040@student.rmit.edu.au>
2023-10-06 15:00:59 +1100

    half of menu done

--- mazeRunner.cpp
+++ mazeRunner.cpp
@@ -28,17 +28,51 @@ int main(void){
     mc.doCommand("time set day"); 
 
     States curState = ST_Main;
+    int userChoice = 0;
+    
 
     //State machine for menu        
     while (curState != ST_Exit)
     {
         std::cout << "Welcome to MineCraft MazeRunner!" << std::endl << "--------------------------------" << std::endl;
-        curState = ST_Exit;
-    }
+        
+        std::cout << "------------- MAIN MENU -------------" << std::endl;
+        std::cout << "1) Generate Maze" << std::endl;
+        std::cout << "2) Build Maze in MineCraft" << std::endl;
+        std::cout << "3) Solve Maze" << std::endl;
+        std::cout << "4) Show Team Information" << std::endl;
+        std::cout << "5) Exit" << std::endl;
+        std::cout << "" << std::endl;
+        std::cout << "Enter Menu item to continue:" << std::endl;
+        std::cin >> userChoice;
 
-    printExitMassage();
+        if (userChoice == 1) {
+            generateMaze();
+        }
+
+        if (userChoice == 2){
+            BuildMaze();
+        }
+
+        
+    }
+     
+    printExitMessage();
 
 
     return EXIT_SUCCESS;
+
+}
+
+static void printExitMessage() {
+    std::cout << "**End Printing Maze**";
+
+}
+
+static void generateMaze() {
+    //generate maze
+}
+
+static void BuildMaze(){
 
 }
