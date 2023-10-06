b45aa8508850a9138d3737f5d83deedad998bbab
Vincent-Daoo <s4007708@student.rmit.edu.au>
2023-10-06 15:40:35 +1100

    menu finsihed (potentially)

--- mazeRunner.cpp
+++ mazeRunner.cpp
@@ -28,13 +28,37 @@ int main(void){
     mc.doCommand("time set day"); 
 
     States curState = ST_Main;
- //   int userChoice = 0;
+    int userChoice = 0;
     
 
     //State machine for menu        
     while (curState != ST_Exit)
     {
+        printStartText();
         printMainMenu();
+        std::cin >> userChoice;
+        if(userChoice == 1 || userChoice == 2 || userChoice == 3 || userChoice == 4 || userChoice == 5){
+            curState = ST_Exit;
+
+            if(userChoice == 1){
+                printGenerateMazeMenu();
+            }
+            else if(userChoice == 2){
+                printGenerateMazeMenu();
+            }
+            else if(userChoice == 3){
+                printSolveMazeMenu();
+            }
+            else if(userChoice == 4){
+                printTeamInfo();
+            }
+            else{
+                printExitMessage();
+            }
+        }
+        else{
+            std::cout << "Error. Input Invalid. Please enter a number from 1 to 5." << std::endl;
+        }
     }
 }
 
