a8e6be8fb72b86945c600452f8049c93f1077a82
Vincent-Daoo <s4007708@student.rmit.edu.au>
2023-10-11 11:08:00 +1100

    Adjusted the white spacing

--- Maze.cpp
+++ Maze.cpp
@@ -2,12 +2,8 @@
 #include <random>
 
 
-Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen, 
-                                    unsigned int zlen,
-                                    bool mode)
-{
+Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen, unsigned int zlen, bool mode){
 }
 
-Maze::~Maze()
-{
+Maze::~Maze(){
 }
